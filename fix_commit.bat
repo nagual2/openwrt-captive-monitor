636ee4fa1d1250622664a1ea4f9be04dab835e0e
nahual15 <nahual15@gmail.com>
2025-11-02 22:28:29 +0100

    \

--- fix_commit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 65001 > nul
-git commit --amend -m "fix: исправление форматирования Markdown и удаление временных файлов"
-git push --force origin main
--- fix_commit_message.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 65001 > nul
-git commit --amend -m "fix: исправление форматирования Markdown файлов" --no-edit
-git push --force origin main
